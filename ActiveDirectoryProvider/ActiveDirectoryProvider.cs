061207cb60b493f353bc3ac12273196d7cd9a37f
Dario Solera <dario.solera@threeplicate.com>
2009-10-09 15:19:13 +0000

    Added ActiveDirectoryProvider project (placeholder).

---
+++ ActiveDirectoryProvider/ActiveDirectoryProvider.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScrewTurn.Wiki.Plugins.ActiveDirectory {
+
+	/// <summary>
+	/// Implements a Users Storage Provider for Active Directory.
+	/// </summary>
+	public class ActiveDirectoryProvider {
+		// Placeholder
+	}
+
+}
