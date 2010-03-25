9696e38c320f2b2925217a788a991796a44b4eab
Matteo Tomasini <matteo.tomasini@threeplicate.com>
2010-03-25 14:05:42 +0000

    Fixed and closed #496 : Added max column(2) for groups list.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -16,5 +16,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.2.512")]
-[assembly: AssemblyFileVersion("3.0.2.512")]
+[assembly: AssemblyVersion("3.0.2.513")]
+[assembly: AssemblyFileVersion("3.0.2.513")]
