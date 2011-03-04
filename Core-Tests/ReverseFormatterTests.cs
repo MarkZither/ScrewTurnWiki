22755484588ca2f445797e2d5c72302c062522e2
Cristian Trapattoni <cristian.trapattoni@threeplicate.com>
2011-03-04 17:54:32 +0100

    Removed anthem library

---
+++ Core-Tests/ReverseFormatterTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ScrewTurn.Wiki.Tests {
+	
+	[TestFixture]
+	public class ReverseFormatterTests {
+
+		[Test]
+		[TestCase("<b>text</b>", "'''text'''")]
+		[TestCase("<strong>text</strong>", "'''text'''")]
+		[TestCase("<i>text</i>", "''text''")]
+		[TestCase("<ul><li>prova <b>pippo</b></li><li>riga2</li></ul>", "* prova '''pippo'''\r\n* riga2\r\n")]
+		[TestCase("<em>text</em>", "''text''")]
+		[TestCase("<u>text</u>", "__text__")]
+		[TestCase("<s>text</s>", "--text--")]
+		[TestCase("<html><table border=\"1\" bgcolor=\"LightBlue\"><thead><tr><th>Cells x.1</th><th>Cells x.2</th></tr></thead><tbody><tr><td>Cell 1.1</td><td>Cell 1.2</td></tr><tr><td>Cell 2.1</td><td>Cell 2.2</td></tr></tbody></table></html>", "{| border=\"1\" bgcolor=\"LightBlue\" \r\n|- \r\n! Cells x.1\r\n! Cells x.2\r\n|- \r\n| Cell 1.1\r\n| Cell 1.2\r\n|- \r\n| Cell 2.1\r\n| Cell 2.2\r\n|}\r\n")]
+		[TestCase("<ol><li><a class=\"internallink\" target=\"_blank\" href=\"www.try.com\" title=\"try\">try</a></li><li><a class=\"internallink\" target=\"_blank\" href=\"www.secondtry.com\" title=\"www.secondtry.com\">www.secondtry.com</a><br></li></ol>","# [^www.try.com|try]\r\n# [^www.secondtry.com|www.secondtry.com]\r\n")]
+		[TestCase("<table><tbody><tr><td bgcolor=\"Blue\">Styled Cell</td><td>Normal cell</td></tr><tr><td>Normal cell</td><td bgcolor=\"Yellow\">Styled cell</td></tr></tbody></table>", "{| \r\n|- \r\n|  bgcolor=\"Blue\"  | Styled Cell\r\n| Normal cell\r\n|- \r\n| Normal cell\r\n|  bgcolor=\"Yellow\"  | Styled cell\r\n|}\r\n")]
+		[TestCase("<h1>text</h1>", "==text==\r\n")]
+		[TestCase("<h2>text</h2>", "===text===\r\n")]
+		[TestCase("<h3>text</h3>", "====text====\r\n")]
+		[TestCase("<h4>text</s>", "=====text=====\r\n")]
+		[TestCase("<code>inline code - monospace font</code>", "{{inline code - monospace font}}")]
+		[TestCase("<h1></h1>", "----\r\n")]
+		[TestCase("<h1> </h1>", "----\r\n")]
+		[TestCase("<sup>text</sup>", "<sup>text</sup>")]
+		[TestCase("<sub>text</sub>", "<sub>text</sub>")]
+		[TestCase("<pre><b>text</b></pre>", "@@text@@")]
+		[TestCase("<div class=\"indent\" style=\"margin: 0px; padding: 0px; padding-left: 15px\">text</div>", ": text\r\n")]
+		[TestCase("<a href=\"Help.AllPages.aspx?Cat=Help.Wiki\" class=\"systemlink\" title=\"Help.Wiki\">Help.Wiki</a>", "[c:Help.Wiki|Help.Wiki]")]
+		[TestCase("<code><b>text</b></code>", "{{'''text'''}}")]
+		[TestCase("<div class=\"box\">text</div>", "(((text)))\r\n")]
+		[TestCase("<div>text</div>", "text\r\n")]
+		[TestCase("<html>riga1\r\n<b>riga2</b>\r\nriga3</html>", "riga1\r\n'''riga2'''\r\nriga3")]
+		[TestCase("<html><ol><li>1</li><li>2</li><li>3<ol><li>3.1</li><li>3.2<ol><li>3.2.1</li></ol></li><li>3.3</li></ol></li><li>4 ciao</li></ol><br /></html>", "# 1\r\n# 2\r\n# 3\r\n## 3.1\r\n## 3.2\r\n### 3.2.1\r\n## 3.3\r\n# 4 ciao\r\n\r\n")]
+		[TestCase("<ol><li>1</li><li>2</li></ol>", "# 1\r\n# 2\r\n")]
+		[TestCase("<ul><li><img src=\"GetFile.aspx?File=/AmanuensMicro.png\" alt=\"Image\"></li><li><div class=\"imageleft\"><img class=\"image\" src=\"GetFile.aspx?File=/DownloadButton.png\" alt=\"Image\"></div></li><li><div class=\"imageright\"><a target=\"_blank\" href=\"www.tututu.tu\" title=\"guihojk\"><img class=\"image\" src=\"GetFile.aspx?File=/Checked.png\" alt=\"guihojk\"></a><p class=\"imagedescription\">guihojk</p></div></li><li><table class=\"imageauto\" cellpadding=\"0\" cellspacing=\"0\"><tbody><tr><td><img class=\"image\" src=\"GetFile.aspx?File=/Alert.png\" alt=\"auto\"><p class=\"imagedescription\">auto</p></td></tr></tbody></table><br></li></ul>", "* [image|Image|{UP}/AmanuensMicro.png]\r\n* [imageleft||{UP}/DownloadButton.png]\r\n* [imageright|guihojk|{UP}/Checked.png|^www.tututu.tu]\r\n* [imageauto|auto|{UP}/Alert.png]\r\n\r\n")]
+		[TestCase("<ul><li>1</li><li>2</li></ul>", "* 1\r\n* 2\r\n")]
+		[TestCase("<div class=\"imageright\"><img class=\"image\" src=\"GetFile.aspx?File=/Help/Desktop/image.png\"><p class=\"imagedescription\">description</p></div>", "[imageright|description|{UP}/Help/Desktop/image.png]\r\n")]
+		[TestCase("<html><ul><li>Punto 1</li><li>Punto 2</li><li>Punto 3</li><li>Punto 4</li><li>Punto 5</li></ul></html>", "* Punto 1\r\n* Punto 2\r\n* Punto 3\r\n* Punto 4\r\n* Punto 5\r\n")]
+		[TestCase("<ul><li>it 1<ul><li>1.1</li><li>1.2</li></ul></li><li>it2</li></ul>", "* it 1\r\n** 1.1\r\n** 1.2\r\n* it2\r\n")]
+		[TestCase("<ul><li>it 1<ol><li>1.1</li><li>1.2</li></ol></li><li>it2</li></ul>", "* it 1\r\n*# 1.1\r\n*# 1.2\r\n* it2\r\n")]
+		[TestCase("<ul><li><b>1</b></li><li>2</li></ul>", "* '''1'''\r\n* 2\r\n")]
+		[TestCase("<html><a id=\"Init\" />I'm an anchor</html>", "[anchor|#init]I'm an anchor")]
+		[TestCase("<html><a class=\"internallink\" href=\"#init\" title=\"This recall an anchor\">This recall an anchor</a></html>", "[#init|This recall an anchor]")]
+		[TestCase("<html><a class=\"externallink\" href=\"google.com\" title=\"BIG TITLE\" target=\"_blank\">BIG TITLE</a></html>", "[^google.com|BIG TITLE]")]
+		[TestCase("<esc>try to esc tag</esc>", "<esc>try to esc tag</esc>")]
+		[TestCase("<div class=\"imageleft\"><a target=\"_blank\" href=\"www.link.com\" title=\"left Align\"><img class=\"image\" src=\"GetFile.aspx?Page=MainPage&File=image.png\" alt=\"left Align\" /></a><p class=\"imagedescription\">leftalign</p></div>", "[imageleft|leftalign|{UP(MainPage)}image.png|^www.link.com]\r\n")]
+		[TestCase("<img src=\"GetFile.aspx?Page=MainPage&File=image.png\" alt=\"inlineimage\" />", "[image|inlineimage|{UP(MainPage)}image.png]\r\n")]
+		[TestCase("<a target=\"_blank\" href=\"www.google.it\" title=\"description\"><img src=\"GetFile.aspx?Page=MainPage&File=image.png\" alt=\"description\" /></a>", "[image|description|{UP(MainPage)}image.png|^www.google.it]\r\n")]
+		[TestCase("<table class=\"imageauto\" cellpadding=\"0\" cellspacing=\"0\"><tbody><tr><td><img class=\"image\" src=\"GetFile.aspx?Page=MainPage&File=image.png\" alt=\"autoalign\" /><p class=\"imagedescription\">autoalign</p></td></tr></tbody></table>", "[imageauto|autoalign|{UP(MainPage)}image.png]\r\n")]
+		[TestCase("<table class=\"imageauto\" cellpadding=\"0\" cellspacing=\"0\"><tbody><tr><td><a href=\"www.link.com\" title=\"Auto align\"><img class=\"image\" src=\"GetFile.aspx?Page=MainPage&File=image.png\" alt=\"Auto align\" /></a><p class=\"imagedescription\">Auto align</p></td></tr></tbody></table>", "[imageauto|Auto align|{UP(MainPage)}image.png|www.link.com]\r\n")]
+		[TestCase("<table cellspacing=\"0\" cellpadding=\"2\" style=\"background-color: #EEEEEE; margin: 0px auto;\"><caption>Styled Table</caption><tbody><tr style=\"background-color: #990000; color: #FFFFFF;\"><td>This is a cell</td><td>This is a cell</td><td>This is a cell</td></tr><tr><td style=\"background-color: #000000; color: #CCCCCC;\">Styled cell</td><td style=\"border: solid 1px #FF0000;\">Styled cell</td><td><b>Normal cell</b></td></tr><tr><td>Normal</td><td>Normal</td><td><a class=\"internallink\" href=\"Download.ashx\" title=\"Download\">Download</a></td></tr></tbody></table>","{| cellspacing=\"0\" cellpadding=\"2\" style=\"background-color: #EEEEEE; margin: 0px auto;\" \r\n|+ Styled Table\r\n|- style=\"background-color: #990000; color: #FFFFFF;\" \r\n| This is a cell\r\n| This is a cell\r\n| This is a cell\r\n|- \r\n|  style=\"background-color: #000000; color: #CCCCCC;\"  | Styled cell\r\n|  style=\"border: solid 1px #FF0000;\"  | Styled cell\r\n| '''Normal cell'''\r\n|- \r\n| Normal\r\n| Normal\r\n| [Download.ashx|Download]\r\n|}\r\n")]
+		[TestCase("<pre>block code - [WikiMarkup] is ignored</pre>", "@@block code - [WikiMarkup] is ignored@@")]
+		public void PlainTest(string input, string output) {
+			Assert.AreEqual(output, ReverseFormatter.ReverseFormat(input));
+		}
+	}
+}
---
+++ Core/ReverseFormatter.cs
@@ -0,0 +1,488 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Xml;
+using System.IO;
+
+namespace ScrewTurn.Wiki {
+
+	/// <summary>
+	/// Implements reverse formatting methods (HTML-&gt;WikiMarkup).
+	/// </summary>
+	public static class ReverseFormatter {
+
+
+		/// <summary>
+		/// Processes order or unorder lists and sublists.
+		/// </summary>
+		/// <param name="nodes">The nodes.</param>
+		/// <param name="marker">The marker.</param>
+		/// <returns>Valid WikiMarkUp text for the lists</returns>
+		private static string processList(XmlNodeList nodes, string marker) {
+			string result = "";
+			string ul = "*";
+			string ol = "#";
+			foreach(XmlNode node in nodes) {
+				string text = "";
+				if(node.Name == "li") {
+					foreach(XmlNode child in node.ChildNodes) {
+						if(child.Name != "ol" && child.Name != "ul") {
+							StringReader a = new StringReader(child.OuterXml);
+							XmlDocument n = FromHTML((TextReader)a);
+							text += processChild(n.ChildNodes);
+						}
+					}
+					result += marker + " " + text;
+					if(!result.EndsWith("\r\n")) result += "\r\n";
+					foreach(XmlNode child in node.ChildNodes) {
+						if(child.Name.ToString() == "ol"){
+							result += processList(child.ChildNodes, marker + ol);
+						}
+						if (child.Name.ToString() == "ul") {
+							result += processList(child.ChildNodes, marker + ul);
+						}
+					}
+				}
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Processes the image.
+		/// </summary>
+		/// <param name="node">The node contenent fileName of the image.</param>
+		/// <returns>The correct path for wikimarup and image</returns>
+		private static string processImage(XmlNode node) {
+			string result = "";
+			if(node.Attributes.Count != 0) {
+				foreach(XmlAttribute attName in node.Attributes) {
+					if(attName.Name == "src")  {
+						string[] path = attName.Value.ToString().Split('=');
+						if(path.Length > 2)
+							result += "{" + "UP(" + path[1].Split('&')[0] + ")}" + path[2];
+						else result += "{UP}" + path[path.Length - 1];
+					}
+				}
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Processes the child Image.
+		/// </summary>
+		/// <param name="nodes">Nodelist from an image.</param>
+		/// <returns>The correct WikiMarkup for the images </returns>
+		private static string processChildImage(XmlNodeList nodes) {
+			string image = "";
+			string p = "";
+			string url = "";
+			string result = "";
+			bool hasDescription = false;
+			foreach(XmlNode node in nodes) {
+				if(node.Name.ToLowerInvariant() == "img")
+					image += processImage(node);
+				else if(node.Name.ToLowerInvariant() == "p") {
+					hasDescription = true;
+					p += "|" + processChild(node.ChildNodes) + "|";
+				}
+				else if(node.Name.ToLowerInvariant() == "a") {
+					string link = "";
+					string target = "";
+
+					if(node.Attributes.Count != 0) {
+						XmlAttributeCollection attribute = node.Attributes;
+						foreach(XmlAttribute attName in attribute) {
+							if(attName.Value.ToString() == "_blank")
+								target += "^";
+							if(attName.Name.ToString() == "href")
+								link += attName.Value.ToString();
+						}
+					}
+					image += processImage(node.LastChild);
+					url = "|" + target + link;
+				}
+			}
+			if (!hasDescription)
+				p = "||";
+			result = p + image + url;
+			return result;
+		}
+
+
+		/// <summary>
+		/// Processes the table image.
+		/// </summary>
+		/// <param name="nodes">The nodes.</param>
+		/// <returns>An auto-image type formmatter</returns>
+		private static string processTableImage(XmlNodeList nodes){
+			string result = "";
+			foreach(XmlNode node in nodes){
+				switch(node.Name.ToLowerInvariant()) {
+					case "tbody":
+						result += processTableImage(node.ChildNodes);
+						break;
+					case "tr":
+						result += processTableImage(node.ChildNodes);
+						break;
+					case "td":
+						string image = "";
+						string aref = "";
+						string p = "";
+						bool hasLink = false;
+						if(node.FirstChild.Name.ToLowerInvariant() == "img") image += processTableImage(node.ChildNodes);
+						if(node.FirstChild.Name.ToLowerInvariant() == "a") {
+							hasLink = true;
+							aref += processTableImage(node.ChildNodes);
+						}
+						if(node.LastChild.Name.ToLowerInvariant() == "p") p += node.LastChild.InnerText.ToString();
+						if(!hasLink) result += p + image;
+						else result += p + aref;
+						break;
+					case "img":
+						result += "|" + processImage(node);
+						break;
+					case "a":
+						string link = "";
+						string target = "";
+						string title = "";
+						if(node.Attributes.Count != 0) {
+							XmlAttributeCollection attribute = node.Attributes;
+							foreach(XmlAttribute attName in attribute) {
+								if(attName.Name.ToString() != "id".ToLowerInvariant()) {
+									if(attName.Value.ToString() == "_blank")
+										target += "^";
+									if(attName.Name.ToString() == "href")
+										link += attName.Value.ToString();
+									if(attName.Name.ToString() == "title")
+										title += attName.Value.ToString();
+								}
+							}
+							result += processTableImage(node.ChildNodes) + "|" + target + link;
+						}
+						break;
+				}
+			}
+			return result;
+		}
+		/// <summary>
+		/// Processes the code.
+		/// </summary>
+		/// <param name="text">The text.</param>
+		/// <returns></returns>
+		private static string processCode(string text) {
+			string result = "";
+			result = text;
+			return result;
+		}
+
+		/// <summary>
+		/// Processes the table.
+		/// </summary>
+		/// <param name="nodes">The nodes.</param>
+		/// <returns></returns>
+		private static string processTable(XmlNodeList nodes) {
+			string result = "";
+
+			foreach(XmlNode node in nodes) {
+				switch (node.Name.ToLowerInvariant()){
+					case "thead":
+						result += processTable(node.ChildNodes);
+						break;
+					case "th":
+						result += "! " + processChild(node.ChildNodes) + "\r\n"; 
+						break;
+					case "caption":
+						result += "|+ "+ processChild(node.ChildNodes) + "\r\n";
+						break;
+					case "tbody":
+						result += processTable(node.ChildNodes) + "";
+						break;
+					case "tr":
+						string style = "";
+						foreach(XmlAttribute attr in node.Attributes) 
+							if(attr.Name.ToLowerInvariant() == "style") style += "style=\"" + attr.Value.ToString() + "\" ";
+
+						result += "|- " + style + "\r\n" + processTable(node.ChildNodes);
+						//else result += processTable(node.ChildNodes);
+						break;
+					case "td":
+						string styleTd = "";
+						if(node.Attributes.Count != 0) { 
+							foreach(XmlAttribute attr in node.Attributes) 
+								styleTd += " " + attr.Name + "=\"" + attr.Value.ToString() + "\" ";
+							result += "| " + styleTd + " | " + processChild(node.ChildNodes) + "\r\n";
+						}
+						else result += "| " + processChild(node.ChildNodes) + "\r\n";
+						break;
+				}	
+
+			}
+			return result;
+		}
+		/// <summary>
+		/// Processes the child.
+		/// </summary>
+		/// <param name="nodes">A XmlNodeList .</param>
+		/// <returns>The corrispondent WikiMarkup Text</returns>
+		private static string processChild(XmlNodeList nodes) {
+			string result = "";
+			foreach(XmlNode node in nodes) {
+				bool anchor = false;
+				if(node.NodeType == XmlNodeType.Text) {
+					result += node.Value;
+				}
+				else if(node.NodeType != XmlNodeType.Whitespace) {
+					switch(node.Name.ToLowerInvariant()) {
+						case "html":
+							result += processChild(node.ChildNodes);
+							break;
+						case "b":
+						case "strong":
+							result += ("'''" + processChild(node.ChildNodes) + "'''");
+							break;
+						case "strike":
+						case "s":
+							result += ("--" + processChild(node.ChildNodes) + "--");
+							break;
+						case "em":
+						case "i":
+							result += ("''" + processChild(node.ChildNodes) + "''");
+							break;
+						case "u":
+							result += ("__" + processChild(node.ChildNodes) + "__");
+							break;
+						//break;
+						case "h1":
+							if(node.HasChildNodes) {
+								if (node.FirstChild.NodeType == XmlNodeType.Whitespace) result += ("----\r\n" + processChild(node.ChildNodes));
+								else result += ("==" + processChild(node.ChildNodes) + "==\r\n");
+							}
+							else result += ("----\r\n");
+							break;
+						//break;
+						case "h2":
+							result += ("===" + processChild(node.ChildNodes) + "===\r\n");
+							break;
+						//break;
+						case "h3":
+							result += ("====" + processChild(node.ChildNodes) + "====\r\n");
+							break;
+						//break;
+						case "h4":
+							result += ("=====" + processChild(node.ChildNodes) + "=====\r\n");
+							break;
+						case "pre":
+							result += ("@@" + processCode(node.InnerText.ToString()) + "@@");
+							break;
+						case "code":
+							result += ("{{" + processChild(node.ChildNodes) + "}}");
+							break;
+						case "hr":
+						case "hr /":
+							result += ("\r\n== ==\r\n" + processChild(node.ChildNodes));
+							break;
+						case "span":
+							if(node.Attributes.Count != 0) {
+								XmlAttributeCollection attribute = node.Attributes;
+								foreach(XmlAttribute attName in attribute) {
+									if(attName.Value.ToString() == "italic")
+										result += "''" + processChild(node.ChildNodes) + "''";
+								}
+							}
+							break;
+						case "br":
+							result += ("\r\n" + processChild(node.ChildNodes));
+							break;
+						case "table":
+							bool isImage = false;
+							string image = "";
+							string tableStyle = "";
+
+							foreach(XmlAttribute attName in node.Attributes) {
+								if(attName.Value.ToString() == "imageauto") {
+									isImage = true;
+									image += "[imageauto|" + processTableImage(node.ChildNodes)  + "]\r\n";
+								}
+								else tableStyle += attName.Name + "=\"" + attName.Value.ToString() + "\" ";
+							}
+							if(isImage) {
+								result += image;
+								isImage = false;
+								break;
+							}
+							else result += "{| " + tableStyle + "\r\n" + processTable(node.ChildNodes) + "|}\r\n";
+							break;
+						case "ol":
+							if(node.ParentNode != null) {
+								if(node.ParentNode.Name.ToLowerInvariant() != "td")	result += processList(node.ChildNodes, "#");
+								else result += node.OuterXml.ToString();
+							}
+							else result += processList(node.ChildNodes, "#");
+							break;
+						case "ul":
+							if (node.ParentNode != null) {
+								if(node.ParentNode.Name.ToLowerInvariant() != "td") result += processList(node.ChildNodes, "*");
+								else result += node.OuterXml.ToString();
+							}
+							else result += processList(node.ChildNodes, "*");
+							break;
+						case "sup":
+							result += ("<sup>" + processChild(node.ChildNodes) + "</sup>");
+							break;
+						case "sub":
+							result += ("<sub>" + processChild(node.ChildNodes) + "</sub>");
+							break;
+						case "p":
+							if(node.Attributes.Count != 0) {
+								XmlAttributeCollection attribute = node.Attributes;
+								foreach(XmlAttribute attName in attribute) {
+									if(attName.Value.ToString() == "imagedescription")
+										result += "";
+								}
+							}
+							else result += processChild(node.ChildNodes) + "{BR}\r\n";
+							break;
+						case "div":
+							if(node.Attributes.Count != 0) {
+								XmlAttributeCollection attribute = node.Attributes;
+								foreach(XmlAttribute attName in attribute) {
+									if(attName.Value.ToString() == "box") {
+										result += "(((" + processChild(node.ChildNodes) + ")))\r\n";
+									}
+									if(attName.Value.ToString() == "imageleft") {
+										result += "[imageleft" + processChildImage(node.ChildNodes) + "]\r\n";
+									}
+									if(attName.Value.ToString() == "imageright")
+										result += "[imageright" + processChildImage(node.ChildNodes) + "]\r\n";
+									if(attName.Value.ToString() == "image")
+										result += "[image" + processChildImage(node.ChildNodes) + "]\r\n";
+									if(attName.Value.ToString() == "indent") result += ": " + processChild(node.ChildNodes) + "\r\n";
+								}
+							}
+							else
+								result += (processChild(node.ChildNodes) + "\r\n");
+							break;
+
+						case "img":
+							string description = "";
+							bool hasClass = false;
+							bool isLink = false;
+							if(node.ParentNode != null)
+								if(node.ParentNode.Name.ToLowerInvariant().ToString() == "a")
+									isLink = true;
+							if(node.Attributes.Count != 0) {
+								foreach(XmlAttribute attName in node.Attributes) {
+									if(attName.Name.ToString() == "alt")
+										description = attName.Value.ToString();
+									//description = attName.Value.ToString();
+									if(attName.Name.ToString() == "class")
+										hasClass = true;
+								}
+							}
+							if((!hasClass) && (!isLink))
+								result += "[image|" + description + "|" + processImage(node) + "]\r\n";
+							else if((!hasClass) && (isLink))
+								result += "[image|" + description + "|" + processImage(node);
+							else
+								result += description + "|" + processImage(node);
+							break;
+
+						case "a":
+							bool isTable = false;
+							string link = "";
+							string target = "";
+							string title = "";
+							bool isInternalLink = false;
+							bool childImg = false;
+							if(node.FirstChild != null) {
+								if(node.FirstChild.Name.ToLowerInvariant() == "img")
+									childImg = true;
+							}
+							if(node.ParentNode.Name.ToLowerInvariant() == "td")
+								isTable = true;
+							if(node.Attributes.Count != 0) {
+								XmlAttributeCollection attribute = node.Attributes;
+								foreach(XmlAttribute attName in attribute) {
+									if(attName.Name.ToString() != "id".ToLowerInvariant()) {
+										if(attName.Value.ToString() == "_blank")
+											target += "^";
+										if(attName.Name.ToString() == "href")
+											link += attName.Value.ToString();
+										if(attName.Name.ToString() == "title")
+											title += attName.Value.ToString();
+										if(attName.Value.ToString() == "SystemLink".ToLowerInvariant())
+											isInternalLink = true;
+									}
+									else {
+										anchor = true;
+										result += "[anchor|#" + attName.Value.ToString().ToLowerInvariant() + "]" + processChild(node.ChildNodes);
+										break;
+									}
+								}
+								if (isInternalLink){
+									string[] splittedLink = link.Split('=');
+									link = "c:" + splittedLink[1];
+								}
+
+								if((!anchor) && (!isTable) && (!childImg))
+									if(title != link)
+										result += "[" + target + link + "|" + processChild(node.ChildNodes) + "]";
+									else
+										result += "[" + target + link + "|" + processChild(node.ChildNodes) + "]";
+								if((!anchor) && (!childImg) && (isTable))
+									result += "[" + target + link + "|" + processChild(node.ChildNodes) + "]";
+								if((!anchor) && (childImg) && (!isTable))
+									result += processChild(node.ChildNodes) + "|" + target + link + "]\r\n";
+							}
+							break;
+
+						default:
+							result += (node.OuterXml);
+							break;
+					}
+				}
+				else result += "";
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Froms the HTML.
+		/// </summary>
+		/// <param name="reader">The reader.</param>
+		/// <returns>valid XML Document</returns>
+		private static XmlDocument FromHTML(TextReader reader) {
+
+			// setup SgmlReader
+			Sgml.SgmlReader sgmlReader = new Sgml.SgmlReader();
+			sgmlReader.DocType = "HTML";
+			sgmlReader.WhitespaceHandling = WhitespaceHandling.All;
+
+			sgmlReader.CaseFolding = Sgml.CaseFolding.ToLower;
+			sgmlReader.InputStream = reader;
+
+			// create document
+			XmlDocument doc = new XmlDocument();
+			doc.PreserveWhitespace = true;
+			doc.XmlResolver = null;
+			doc.Load(sgmlReader);
+			return doc;
+		}
+
+		/// <summary>
+		/// Reverse formats HTML content into WikiMarkup.
+		/// </summary>
+		/// <param name="html">The input HTML.</param>
+		/// <returns>The corresponding WikiMarkup.</returns>
+		public static string ReverseFormat(string html) {
+
+			StringReader strReader = new StringReader(html);
+			XmlDocument x = FromHTML((TextReader)strReader);
+			return processChild(x.FirstChild.ChildNodes);
+		}
+	}
+}
