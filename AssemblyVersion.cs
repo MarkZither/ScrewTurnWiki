83ce7dce1c7f973a4758f9bf361f582612657307
Dario Solera <dario.solera@threeplicate.com>
2011-05-24 14:09:49 +0200

    Fixed old hack for "global" resource files.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Threeplicate Srl")]
 [assembly: AssemblyProduct("ScrewTurn Wiki")]
-[assembly: AssemblyCopyright("Copyright © Threeplicate Srl 2006-2010")]
+[assembly: AssemblyCopyright("Copyright © Threeplicate Srl 2006-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,5 +16,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.4.569")]
-[assembly: AssemblyFileVersion("3.0.4.569")]
+[assembly: AssemblyVersion("3.0.4.570")]
+[assembly: AssemblyFileVersion("3.0.4.570")]
