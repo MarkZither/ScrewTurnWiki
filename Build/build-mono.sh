f547ecc92f15835b13591aad18858dd1909d4e99
Marek Habersack <grendel@twistedcode.net>
2011-07-13 00:43:00 +0200

    [mono] Fix build script to create correct favicon symlink

--- Build/build-mono.sh
+++ Build/build-mono.sh
@@ -2,7 +2,7 @@
 xbuild /property:Configuration=Debug ScrewTurnWiki.msbuild
 pushd Artifacts/WebApplication-SqlServer/Themes/MonoClearlyModern
 ln -sf themes/default-fiber.css color-theme.css
-ln -sf images/favicon.gif Icon_Xamarin.gif
+ln -sf images/favicon.gif Icon.Xamarin.gif
 popd
 pushd ../Mono/Plugins/MediaWikiCompat
 ./build.sh
