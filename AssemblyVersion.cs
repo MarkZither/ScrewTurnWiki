287b8e24f4c995486d5f65e5027596b3ff174182
Matteo Tomasini <matteo.tomasini@threeplicate.com>
2011-09-26 10:07:22 +0200

    Fixed issue in backup pages revision.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -16,5 +16,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.4.123")]
-[assembly: AssemblyFileVersion("4.0.4.123")]
+[assembly: AssemblyVersion("4.0.4.124")]
+[assembly: AssemblyFileVersion("4.0.4.124")]
