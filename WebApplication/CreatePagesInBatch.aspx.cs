4e284e95cf797f447bd0133cae37815a0480ea37
Matteo Tomasini <matteo.tomasini@threeplicate.com>
2011-09-26 09:20:26 +0200

    Fixed backup of old revision of a page.

--- WebApplication/CreatePagesInBatch.aspx.cs
+++ WebApplication/CreatePagesInBatch.aspx.cs
@@ -10,7 +10,10 @@ namespace ScrewTurn.Wiki {
 	public partial class CreatePagesInBatch : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
 			for(int i = 0; i < 1000; i++) {
-				Pages.SetPageContent("test1", "", "pagetest" + i, "Title of Pahe Test " + i, "testuser", DateTime.Now, "", "Content of the test page " + i, new string[0], "", SaveMode.Normal);
+				PageContent page = Pages.SetPageContent("root", "", "pagetest" + i, "Title of Pahe Test " + i, "testuser", DateTime.Now, "", "Content of the test page " + i, new string[0], "", SaveMode.Normal);
+				for(int j = 0; j < 200; j++) {
+					Pages.SetPageContent("root", "", NameTools.GetLocalName(page.FullName), page.Title, page.User, DateTime.Now, "", "Content of test page" + i + " at revision " + j, page.Keywords, page.Description, SaveMode.Backup);
+				}
 			}
 		}
 	}
