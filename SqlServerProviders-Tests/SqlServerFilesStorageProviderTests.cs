60c2a6e875d948f47c359ff562d6b6abfc650edb
MarkZither <mark.burton@zither-it.co.uk>
2019-02-24 11:20:48 +0100

    Check for Appveyor Env variable to change connstring

--- SqlServerProviders-Tests/SqlServerFilesStorageProviderTests.cs
+++ SqlServerProviders-Tests/SqlServerFilesStorageProviderTests.cs
@@ -14,7 +14,7 @@ namespace ScrewTurn.Wiki.Plugins.SqlServer.Tests {
 	public class SqlServerFilesStorageProviderTests : FilesStorageProviderTestScaffolding {
 
 		//private const string ConnString = "Data Source=(local)\\SQLExpress;User ID=sa;Password=password;";
-		private const string ConnString = "Data Source=(local)\\MSSQLSERVER2016;Integrated Security=SSPI;";
+		private string ConnString = "Data Source=(local)\\MSSQLSERVER2016;Integrated Security=SSPI;";
 		private const string InitialCatalog = "Initial Catalog=ScrewTurnWikiTest;";
 
 		public override IFilesStorageProviderV30 GetProvider() {
@@ -25,6 +25,10 @@ namespace ScrewTurn.Wiki.Plugins.SqlServer.Tests {
 
 		[OneTimeSetUp]
 		public void FixtureSetUp() {
+			bool.TryParse(Environment.GetEnvironmentVariable("APPVEYOR"), out bool isAppveyor);
+			if(isAppveyor){
+				ConnString = "Server=(local)\\SQL2016;User ID=sa;Password=Password12!";
+			}
 			// Create database with no tables
 			SqlConnection cn = new SqlConnection(ConnString);
 			cn.Open();
